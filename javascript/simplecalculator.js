a83943e32735031fe1672fa262c7cfc3f7ecb2cc
Rishi-atgondan <rishijeeva13@gmail.com>
2021-11-29 21:05:50 +0530

    modified

--- javascript/simplecalculator.js
+++ javascript/simplecalculator.js
@@ -1,21 +1,17 @@
 
 function dis(x) {
-    let element = document.getElementById("input");
-    if (element.value == 0) {
-        element.value = x;
-    } else {
-        element.value = element.value + x;
-    }
+    const element = document.getElementById("input");
+    element.value += x;
 }
 
 function solve() {
-    let element = document.getElementById("input")
+    const element = document.getElementById("input")
     let x = element.value;
     element.value = eval(x);
 }
 
 function clean() {
-    document.getElementById("input").value = 0;
+    document.getElementById("input").value = "";
 }
 
 
